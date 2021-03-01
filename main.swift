9a2548693434cf31d9869472cf3075c25060a30f
Angelo-05 <79872718+Angelo-05@users.noreply.github.com>
2021-03-01 18:30:12 -0300

    primeira atividade swift

---
+++ main.swift
@@ -0,0 +1,31 @@
+//1
+var num: Int
+num = 5
+var total: Int = 6
+var resultado = 7
+
+//2
+var carName: String = "carro"
+var carCode: Int = 2581705
+var preco: Double = 35.767
+var clienteNome: String = "Joana"
+var enderecoCliente = "rua a"
+
+//3
+
+//Escreve comentários em Swift e outros tipos de variáveis ​​de recebimento como strings, inteiros, entre outros
+
+//4
+
+var int: Int = 2
+var double: Double = 2.2
+var text = String(double) + " " + String(int)
+print (text)
+
+//5
+
+//safety:
+//É uma linguagem que o incentiva a especificar os tipos de variáveis.
+
+//inference:
+//O tipo da variável não é especificado, e a linguagem irá deduzir o tipo da variável com base no valor seguido.
